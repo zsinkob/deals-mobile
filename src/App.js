81de0683aae20ff458d368aeaa1b613e93b43cf6
Balazs Zsinko <balazs.zsinko@tesco.com>
2022-06-16 11:04:34 +0200

    Revert "profile"
    This reverts commit b6707f80b7335df36df8d721cddd6431b3578d36.

--- src/App.js
+++ src/App.js
@@ -1,29 +1,13 @@
-import React, {useEffect, useState} from 'react';
+import React from 'react';
 import './App.css';
 import DealList from "./pages/DealList";
 import Notification from "./components/Nofitication";
-import axios from "axios";
-import Profile from "./pages/Profile";
 
-function App(props) {
-
-    let [profile, setProfile] = useState();
-
-    useEffect(() => {
-        if (!profile) {
-            axios.get(`https://randomuser.me/api/`)
-                .then(res => {
-                    console.dir(res);
-                    const profile = res.data.results[0];
-                    setProfile(profile);
-                })
-        }
-    })
+function App() {
 
     return (
         <div className="App">
-            <Notification/>
-            {profile ? <Profile profile={profile}/> : null}
+            <Notification />
             <header className="App-header">
                 <DealList/>
             </header>
